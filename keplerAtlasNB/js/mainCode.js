6d92a4cc905d6d9e3f91a1dbc85cdb9b6c7d13f1
Caroline Klewinowski <caroline.klewinowski@gmail.com>
2018-03-15 18:47:57 -0400

    Worked on galaxy view button and put in NB's legend

--- keplerAtlasNB/js/mainCode.js
+++ keplerAtlasNB/js/mainCode.js
@@ -194,11 +194,88 @@ function earthView() {
 				  //.attr('centerOfRotation', "0 0 0"); //Catherine's version
 				  .attr('centerOfRotation', "-18.345760512980718 500.9411560290405 706.9935215264597"); //Caroline's version
 
-				 planets.attr('radius', function(d) {return d.koi_srad;}); //draw spheres to represent points
+				 planets.attr('radius', function(d) {return 2*d.koi_srad;}); //draw spheres to represent points
 
 				}
 
+function galaxyView() { 
+				//var view_pos = [-1677.11173, 2131.25139, -5981.29509]; //Catherine's version
+				var view_pos = [-71481.90522, -19085.21779, -64575.3209463]; //Caroline's version
+				var fov = 1.0;
+				//var view_or = [0.72929, 0.67504, -0.11162, 3.87833]; //Catherine's version
+				var view_or = [0.87137, -0.35927, -0.33412, 2.68349]; //Caroline's version
 
+
+				viewpoint.attr("position", view_pos.join(" "))
+				  .attr("orientation", view_or.join(" "))
+				  .attr("fieldOfView", fov)
+				  //.attr('centerOfRotation', "0 0 0"); //Catherine's version
+				  .attr('centerOfRotation', "0 0 0"); //Caroline's version
+
+				 planets.attr('radius', function(d) {return 2*d.koi_srad;}); //draw spheres to represent points
+
+				}
+
+function createLegend() {
+		var legendRectSize = 7;                                  
+		var legendSpacing = 20;                                    
+
+		var stellarClass = [
+			  { "sClassName": 'A [7500K - 10000K]', "sClass": "A", "color": "#AADBEA" },
+			  { "sClassName": 'F [6000K - 7500K]', "sClass": "F", "color": "#F6F3D0" },
+			  { "sClassName": 'G [5200K - 6000K]', "sClass": "G", "color": "#F9F862" },
+			  { "sClassName": 'K [3700K - 5200K]', "sClass": "K", "color": "#FBA10D" },
+			];	
+
+		//Initiate container around Legend
+		var legendContainer = svg.append("g").attr("class","legendContainer")
+			  .attr("transform", "translate(" + 30 + "," + (y - 90) + ")");
+		//Create title of Legend
+		var legendTitle = legendContainer.append('text')                                     
+			  .attr('x', 0)              
+			  .attr('y', legendRectSize - legendSpacing)  
+			  .attr("dy", "1em")
+			  .attr('class', 'legendTitle')
+			  .attr('transform', function() {                     
+				var height = legendRectSize + legendSpacing;          
+				var offset =  height * stellarClass.length / 2;     
+				var horz = -2 * legendRectSize;                       
+				var vert = -2.3 * height - offset;                       
+				return 'translate(' + horz + ',' + vert + ')';        
+			  })
+			  .text("Stellar class of the star around which the planet orbits")
+			  .call(wrap, 220);
+			  
+		//Create container per circle/text pair  
+		var legend = legendContainer
+			  .selectAll('.legend')  	
+			  .data(stellarClass)                              
+			  .enter()                                                
+			  .append('g')   
+			  .attr('class', 'legend')                                
+			  .attr('transform', function(d, i) {                     
+				var height = legendRectSize + legendSpacing;          
+				var offset =  height * stellarClass.length / 2;     
+				var horz = -2 * legendRectSize;                       
+				var vert = i * height - offset;                       
+				return 'translate(' + horz + ',' + vert + ')';        
+			  })
+			  .on("mouseover", classSelect(0.02))
+			  .on("mouseout", classSelect(0.6));                                                     
+			//Append circles to Legend
+			legend.append('circle')                                     
+			  .attr('r', legendRectSize)  
+			  .attr('cx', 4)              
+			  .attr('cy', legendRectSize/2 - legendSpacing)
+			  .attr("opacity", 0.5)  		  
+			  .style('fill', function(d) {return d.color;});                                  
+			//Append text to Legend
+			legend.append('text')                                     
+			  .attr('x', legendRectSize + legendSpacing/2)              
+			  .attr('y', legendRectSize - legendSpacing)  
+			  .text(function(d) { return d.sClassName; });  
+	  
+};
 
 /*var planetContainer = container.append("g").attr("class","planetContainer");
 var planets = planetContainer.selectAll("g.planet")
