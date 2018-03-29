22b441e341f1f4a5321d0d69eb692051dea11b29
James Hamue <jhamue@leaders6-12.org>
2018-03-29 18:44:23 -0400

    Cleaned up main code comments. Changed naming conventions for consistency and accuracy. Made scales.js. Edited BSCrscale and BSCcolor scale to more accurately represent the stars in a visually appealing way.

--- keplerAtlasNB/js/mainCode.js
+++ keplerAtlasNB/js/mainCode.js
@@ -31,17 +31,10 @@ var resolution = 1, //sets behavior or animation orbit
 	au = 149597871, //km
 	radiusSun = 695800, //km
 	radiusJupiter = 69911, //km
-	phi = 0, //rotation of ellipses
-	radiusSizer = 6, //Size increaser of radii of planets
-	planetOpacity = 0.6;
-
-//Create SVG
-/*var svg = d3.select("#planetarium").append("svg")
-	.attr("width", x)
-	.attr("height", y);*/
+	phi = 0 //rotation of ellipses
 
 //In the html code, we've created an object of ID "chartholder" within <x3d> tags. Here, we set the dimensions of that object. -ES
-var x3d = d3.select("#chartholder")
+var x3d = d3.select("#chartholder") 
             .attr("width", x + 'px')
             .attr("height", y +'px')
             .attr("showLog", 'true')
@@ -104,145 +97,9 @@ scene.append('group')
     .attr('class', 'zAxis')
     .call(zAxis)
     ;//.select('.domain'); //parallel lines in x vs z plane
-
-//Create a container for everything with the centre in the middle
-//var container = svg.append("g").attr("class","container")
-//					.attr("transform", "translate(" + x/2 + "," + y/2 + ")")
   
-///////////////////////////////////////////////////////////////////////////
-//////////////////////////// Create Scales ////////////////////////////////
-///////////////////////////////////////////////////////////////////////////
-
-
-//Create color gradient for planets based on the temperature of the star that they orbit
-var colors = ["#9C1E1E","#D62828","#E16262","#F3C4C4","#738E9B","#45687A","#2E556A","#174259","#001F2F"];
-var colorScale = d3.scale.linear()
-	 .domain([2000, 3000, 4000, 5000, 6000, 7000, 8000, 9000, 10000]) // Temperatures
-	 .range(colors);
-	
-var opacityScale = d3.scale.linear()	
-	.domain([0, 1000])
-	.range([0, 1]);
-
-function return_vmagnitude_minmax(planets){
-	var currentMinimum = 1000000;
-	var currentMaximum = 0;
-	var currentVmagnitude;
-
-	for(i=0; i<brightstars.length; i++){
-		currentVmag = brightstars[i].Vmagnitude;
-
-		if(currentVmag< currentMinimum){
-			currentMinimum = currentVmag;
-		}
-
-		if(currentVmag > currentMaximum){
-			currentMaximum = currentVmag;
-		}
-
-	}
-	return [currentMinimum, currentMaximum];
-	
-	}
-vmagMin = return_vmagnitude_minmax(brightstars)[0]
-vmagMax = return_vmagnitude_minmax(brightstars)[1]
-
-var vmagScale = d3.scale.linear()	
-	.domain([vmagMin, vmagMax])
-	.range([6000, 3000]);
-
-//Set scale for radius of circles
-//new function to find minimum and maximum stellar radius across the entire data set -James
-function return_radius_minmax(planets){
-	var currentMinimum = 1000000;
-	var currentMaximum = 0;
-	var currentRadius;
-
-	for(i=0; i<planets.length; i++){
-		currentRadius = planets[i].koi_srad;
-
-		if(currentRadius < currentMinimum){
-			currentMinimum = currentRadius;
-		}
-
-		if(currentRadius > currentMaximum){
-			currentMaximum = currentRadius;
-		}
-
-	}
-	return [currentMinimum, currentMaximum];
-	
-	}
-
-radMin = return_radius_minmax(planets)[0] //get minimum and maximum radii -James
-radMax = return_radius_minmax(planets)[1]
-var rScale = d3.scale.linear()
-	.domain([radMin, radMax])
-	.range([5, 30]); //james
-
-//set scale for size of Bright Star Catalog stars -James & Chris
-function return_vmagnitude_minmax(brightstars){
-	var currentMinimum = 1000000;
-	var currentMaximum = 0;
-	var currentVmagnitude;
-
-	for(i=0; i<brightstars.length; i++){
-		currentVmag = brightstars[i].Vmagnitude;
-
-		if(currentVmag< currentMinimum){
-			currentMinimum = currentVmag;
-		}
-
-		if(currentVmag > currentMaximum){
-			currentMaximum = currentVmag;
-		}
-
-	}
-	return [currentMinimum, currentMaximum];
-	
-	}
-vmagMin = return_vmagnitude_minmax(brightstars)[0]
-vmagMax = return_vmagnitude_minmax(brightstars)[1]
-
-var vmagScale = d3.scale.linear()	
-	.domain([vmagMin, vmagMax])
-	.range([6000, 3000]);
-
-//scale x and y "axes"
-var xScale = d3.scale.linear()
-    .domain([0, 15000])
-    .range([0,15000]);
-var yScale = d3.scale.linear()
-    .domain([0, 15000])
-    .range([0,15000])
-var zScale = d3.scale.linear()
-    .domain([0, 15000])
-    .range([0,15000]);
-
 //Format with 2 decimals
 var formatSI = d3.format(".2f");
-
-//Create the gradients for the planet fill
-var gradientChoice = "Temp";
-// createGradients();
-
-///////////////////////////////////////////////////////////////////////////
-/////////////////////////// Plot and move planets /////////////////////////
-///////////////////////////////////////////////////////////////////////////
-
-//Drawing a line for the orbit
-// var orbitsContainer = container.append("g").attr("class","orbitsContainer"); // append "g" to "container" and give it class "orbitsContainer"
-// var orbits = orbitsContainer.selectAll("g.orbit") //select everything of type g with class "orbit"
-// 				.data(planets).enter().append("ellipse") //attaches planets data to currently empty selection; enters data into selection; appends an ellipse for each planet
-// 				.attr("class", "orbit") //give the ellipse class "orbit"
-// 				.attr("cx", function(d) {return d.cx;}) //set x-position of center
-// 				.attr("cy", function(d) {return d.cy;}) //set y-position of center
-// 				.attr("rx", function(d) {return d.major;}) //set major axis
-// 				.attr("ry", function(d) {return d.minor;}) //set minor axis
-// 				.style("fill", "#3E5968")
-// 				.style("fill-opacity", 0)
-// 				.style("stroke", "white")
-// 				.style("stroke-opacity", 0);	
 
 //Drawing BSC	
 var brightstars = scene.selectAll(".brightstars")
@@ -258,13 +115,13 @@ var brightstars = scene.selectAll(".brightstars")
 
             		return xScale(x) + ' ' + yScale(y) + ' ' + zScale(z);})
             	.append('shape')
-            	.call(makeSolid, function(d) {return "Gold"})
+            	.call(makeSolid, function(d) {return vmagcolorscale(d.Vmagnitude)})
             	.append('sphere')
-            	.attr('radius', function(d) {return 1});
+            	.attr('radius', function(d) {return vmagRscale(d.Vmagnitude)});
 
 //Drawing the planets			
-var planets = scene.selectAll(".planet")
-				.data(planets)
+var keplerstars = scene.selectAll(".keplerstars")
+				.data(keplerstars)
             	.enter()
             	.append('transform')
             	.attr('class', 'point')
@@ -277,28 +134,9 @@ var planets = scene.selectAll(".planet")
 
             		return xScale(x) + ' ' + yScale(y) + ' ' + zScale(z);})
             	.append('shape')
-            	.call(makeSolid, function(d) {return colorScale(d.koi_steff)}) //uses a function to return the STeff and apply our color scale to create differences 
+            	.call(makeSolid, function(d) {return keplerstarscolorScale(d.koi_steff)}) //uses a function to return the STeff and apply our color scale to create differences 
             	.append('sphere')
             	.attr('radius', function(d) {return 0.25*rScale(d.koi_srad)}); //draw spheres to represent points, using a function to return the radius and apply the radius scale
-
-//Drawing BSC	
-var brightstars = scene.selectAll(".brightstars")
-				.data(brightstars)
-            	.enter()
-            	.append('transform')
-            	.attr('class', 'point')
-            	.attr('translation', function(d){ 
-            		var xyz = convertXYZ(distance=d.dist, xyzinputRA=d.RA, xyzinputdec=d.dec);
-					var x = xyz[0];
-					var y = xyz[1];
-					var z = xyz[2];
-
-            		return xScale(x) + ' ' + yScale(y) + ' ' + zScale(z);})
-            	.append('shape')
-            	.call(makeSolid, function(d) {return "Gold"})
-            	.append('sphere')
-            	.attr('radius', function(d) {return 1});
-
 
 //add cylinder to the site - Catherine & Caroline
 //note: we need to rotate this so that the cylinder lies in the xy-plane insead pf the xz-plane! -Emily
@@ -330,9 +168,6 @@ function earthView() {
 				  .attr('zNear', zN)
   				  .attr('zFar', zF)
   				  .attr("fieldOfView", fov);
-
-				//planets.attr('radius', function(d) {return 0.25*rScale(d.koi_srad);})
-
 				}
 
 function galaxyView() {
@@ -349,48 +184,4 @@ function galaxyView() {
 				  .attr('centerOfRotation', "0 0 0")
 				  .attr('zNear', zN)
   				  .attr('zFar', zF)
-  				  
-				//planets.attr('radius', function(d) {return 1.5*rScale(d.koi_srad);}) //james 
-
 				}
-/*var planetContainer = container.append("g").attr("class","planetContainer");
-var planets = planetContainer.selectAll("g.planet")
-				.data(planets).enter()
-				//.append("g")
-				//.attr("class", "planetWrap")					
-				.append("circle")
-				.attr("class", "planet")
-				// .attr("r", function(d) {return radiusSizer*d.Radius;})//rScale(d.Radius);})
-				// .attr("cx", function(d) {return d.x;})											//doesn't work because we don't have the data to plot
-				// .attr("cy", function(d) {return d.y;})
-				.attr("r",  function(d) {return d.koi_srad;}) //set radius to d.koi_srad
-				.attr("cx", function(d) {
-					var x = convertXYZ(distance=d.dist, xyzinputRA=d.ra, xyzinputdec=d.dec)[0]
-					//console.log(x);
-					return xScale(x);}) 	//"d" = the planet I'm currently on, in the implicit for-loop
-				.attr("cy", function(d) {
-					var y = convertXYZ(distance=d.dist, xyzinputRA=d.ra, xyzinputdec=d.dec)[1];
-					//console.log(y);
-					return yScale(y);})
-				.style("fill", function(d) {return colorScale(d.koi_steff)}) //d.koi_steff
-				// .style("fill", function(d){return "url(#gradientRadial-" + d.ID + ")";}) 		//no more d.ID
-					// .style("opacity", function(d) {
-					// 	var z = convertXYZ(distance=d.dist, xyzinputRA=d.ra, xyzinputdec=d.dec)[2];
-					// 	// console.log(z)
-					// 	return opacityScale(z);})
-				.style("opacity", .6)
-				.style("stroke-opacity", 0)//depend on z 
-				.style("stroke-width", "3px")
-				.style("stroke", "white")
-				// .on("mouseover", function(d, i) {		/relies on showEllipse function in helperFunctions.js
-				// 	stopTooltip = false					
-				// 	showTooltip(d);
-				// 	showEllipse(d, i, 0.8);
-				// })
-				// .on("mouseout", function(d, i) {
-				// 	showEllipse(d, i, 0);
-				// });
-
-//Remove tooltip when clicking anywhere in body
-d3.select("svg")
-	.on("click", function(d) {stopTooltip = true;});*/
---
+++ keplerAtlasNB/js/scale.js
@@ -0,0 +1,87 @@
+///////////////////////////////////////////////////////////////////////////
+//////////////////////////// Create Scales ////////////////////////////////
+///////////////////////////////////////////////////////////////////////////
+
+
+//Create color gradient for stars based on their size and temperature
+var keplerstarscolors = ["#9C1E1E","#D62828","#E16262","#F3C4C4","#738E9B","#45687A","#2E556A","#174259","#001F2F"];
+var keplerstarscolorScale = d3.scale.linear()
+	 .domain([2000, 3000, 4000, 5000, 6000, 7000, 8000, 9000, 10000]) // Temperatures
+	 .range(keplerstarscolors);
+	
+var kepleropacityScale = d3.scale.linear()	
+	.domain([0, 1000])
+	.range([0, 1]);
+
+//Set scale for radius of circles
+//new function to find minimum and maximum stellar radius across the entire data set -James
+function return_radius_minmax(keplerstars){
+	var currentMinimum = 1000000;
+	var currentMaximum = 0;
+	var currentRadius;
+
+	for(i=0; i<keplerstars.length; i++){
+		currentRadius = keplerstars[i].koi_srad;
+
+		if(currentRadius < currentMinimum){
+			currentMinimum = currentRadius;
+		}
+
+		if(currentRadius > currentMaximum){
+			currentMaximum = currentRadius;
+		}
+
+	}
+	return [currentMinimum, currentMaximum];
+	
+	}
+
+radMin = return_radius_minmax(keplerstars)[0] //get minimum and maximum radii -James
+radMax = return_radius_minmax(keplerstars)[1]
+var rScale = d3.scale.linear()
+	.domain([radMin, radMax])
+	.range([5, 30]); //james
+
+//set scale for size of Bright Star Catalog stars -James & Chris
+function return_vmagnitude_minmax(brightstars){
+	var currentMinimum = 1000000;
+	var currentMaximum = 0;
+	var currentVmagnitude;
+
+	for(i=0; i<brightstars.length; i++){
+		currentVmag = brightstars[i].Vmagnitude;
+
+		if(currentVmag< currentMinimum){
+			currentMinimum = currentVmag;
+		}
+
+		if(currentVmag > currentMaximum){
+			currentMaximum = currentVmag;
+		}
+
+	}
+	return [currentMinimum, currentMaximum];
+	
+	}
+vmagMin = return_vmagnitude_minmax(brightstars)[0]
+vmagMax = return_vmagnitude_minmax(brightstars)[1]
+
+var vmagRscale = d3.scale.linear()	
+	.domain([vmagMin, vmagMax])
+	.range([1.5, 1]);
+
+var vmagcolors = ["#FFF585", "#FFFCEA", "#FFFEFD"];
+var vmagcolorscale = d3.scale.linear()
+	.domain([vmagMin, vmagMax])
+	.range(vmagcolors);
+
+//scale x and y "axes"
+var xScale = d3.scale.linear()
+    .domain([0, 15000])
+    .range([0,15000]);
+var yScale = d3.scale.linear()
+    .domain([0, 15000])
+    .range([0,15000])
+var zScale = d3.scale.linear()
+    .domain([0, 15000])
+    .range([0,15000]);
