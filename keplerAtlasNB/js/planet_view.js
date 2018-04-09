9e02cc3e9a7ee617d5f62b732224c358b75556d5
Emily Sandford <esandford@astro.columbia.edu>
2018-04-09 15:47:23 -0400

    Compiled changes from last time & messed with the viewpoint a little bit

--- keplerAtlasNB/js/planet_view.js
+++ keplerAtlasNB/js/planet_view.js
@@ -36,15 +36,14 @@ d3.select('.x3dom-canvas') //creates a canvas to hold the 3d objects
 
 //starts camera at ideal viewpoint
 var scene = x3d.append("scene");
-var view_pos = [4012.04714, 1604.51702, 4508.39866]; //x, y, z relative to origin (0, 0, 0)
-//var view_pos = [-37902.27708, -31717.63386, -17253.83076]; //new view_pos and fov -Chris
+var view_pos = [109638.27886, -87882.84581, 10160.65712]; //x, y, z relative to origin (0, 0, 0)
 var fov = 0.05; 	// Preferred minimum viewing angle from this viewpoint in radians. 
 				// Small field of view roughly corresponds to a telephoto lens, 
 				// large field of view roughly corresponds to a wide-angle lens. 
 				// Hint: modifying Viewpoint distance to object may be better for zooming. 
 				// Warning: fieldOfView may not be correct for different window sizes and aspect ratios. 
 
-var view_or = [-0.35747, 0.93387, 0.01010, 0.76136]; //relative to default (0, 0, 1, 0)
+var view_or = [0.81848, 0.38215, 0.42901, 1.70872]; //relative to default (0, 0, 1, 0)
 var zN = 0; 		//near plane
 var zF = 150000;	//far plane
 
@@ -85,7 +84,6 @@ scene.append('group')
     .select('.domain').call(makeSolid, 'red', 1.0); //parallel lines in y vs x plane
   
 
-
 var drawn_star = scene.selectAll(".keplerstar")
 							 .data(keplerstars)
             				 .enter()
@@ -93,21 +91,47 @@ var drawn_star = scene.selectAll(".keplerstar")
             				 .attr('class', 'point')
             				 .attr('translation', '0 0 0')
             				 .append('shape')
-            				 .call(makeSolid, color="red", opacity = .6) //uses a function to return the STeff and apply our color scale to create differences 
+                     .call(makeSolid, color=function(d){return keplerstarscolorScale(d.koi_steff)}, opacity = .8) //uses a function to return the STeff and apply our color scale to create differences 
+                     //.append("appearance")
+                     //.append("material")
+                     //.attr("diffuseColor", "red")
+                     /*.attr("transparency", function(d){
+                        var opacity;
+                        if (d.kepoi_name.slice(-1) == 1){
+                          opacity = 0.8;
+                        }
+                        else{
+                          opacity = 0;
+                        }
+                        console.log(opacity);
+                        console.log(1-opacity);
+                      return (1 - opacity);})*/ //doesn't currently work
+                     //.attr("transparency",0)
             				 .append('sphere')
-            				 .attr('radius', function(d) {return 2*rScale(d.koi_srad)}); //draw spheres to represent points, using a function to return the radius and apply the radius scale
-
+                     .attr('radius', function(d) {return 2*pScale(d.koi_srad)});
 
 var drawn_planet = scene.selectAll(".keplerstar")
 							 .data(keplerstars)
             				 .enter()
             				 .append('transform')
             				 .attr('class', 'point')
-            				 .attr('translation', function(d){ 
-            			
-            					 return 3000*d.koi_sma + ' ' + 0 + ' ' + 0;})
+            				 .attr('translation', function(d){return smaScale(d.koi_sma) + ' ' + 0 + ' ' + 0;})
             				 .append('shape')
-            				 .call(makeSolid, color="blue", opacity = 1) //uses a function to return the STeff and apply our color scale to create differences 
+                     .call(makeSolid, color=function(d){return keplerplanetcolorScale(d.koi_teq)}, opacity = 1) //uses a function to return the STeff and apply our color scale to create differences 
+            				 //.call(makeSolid, color="blue", opacity = 1) //uses a function to return the STeff and apply our color scale to create differences 
             				 .append('sphere')
-            				 .attr('radius', function(d) {return 2*rScale(d.koi_ror * d.koi_srad)}); //draw spheres to represent points, using a function to return the radius and apply the radius scale
+            				 .attr('radius', function(d) {return rorScale(d.koi_ror * d.koi_srad)}); //draw spheres to represent points, using a function to return the radius and apply the radius scale
+
+var orbit = scene.selectAll(".orbits")   //creates a selection, which is currently empty
+          .data(keplerstars)        //join "circles" list
+          .enter()          //enter "circles" into empty selection. the selection now contains all of "circles", and everything after this loops over each circle in turn
+          .append('transform')    //for each circle, append a "transform" object
+          .attr('translation', '0 0 0')   //specify that this "transform" will impose a rotation of the circle
+          .attr('rotation', '1 0 0')
+          .append('shape')          //for each circle, append an as-yet-unspecified shape to be drawn on our 3D canvas
+          .call(makeSolid, color='black', opacity=1)       //set the color
+                .append('Circle2D')         //make the shape a 2D circle
+          .attr('radius', function(d){return smaScale(d.koi_sma);})  //set the radius
+          .attr('subdivision',100)      //set the"resolution" of the circle, i.e. how many line segments are drawn to make up the circle
+
 
