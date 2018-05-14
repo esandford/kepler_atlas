a29e0ad05e314dd41b6669ca0433f1d6ec39b7b9
Emily Sandford <esandford@astro.columbia.edu>
2018-05-14 14:00:13 -0400

    Compiled changes from last week—James & Chris’ work on zoom.js; Catherine’s work on html structure

--- keplerAtlasNB/js/zoom.js
+++ keplerAtlasNB/js/zoom.js
@@ -1,8 +1,10 @@
+var margin = {top: -5, right: -5, bottom: -5, left: -5},
+    width = 960 - margin.left - margin.right,
+    height = 500 - margin.top - margin.bottom;
+
 var zoom = d3.behavior.zoom()
     .scaleExtent([1, 10])
     .on("zoom", zoomed);
-
-console.log(zoom.scaleExtent()[0])
 
 var drag = d3.behavior.drag()
     .origin(function(d) { return d; })
@@ -26,6 +28,29 @@ var svg = d3.select("body").append("svg")
     .attr("transform", "translate(" + margin.left + "," + margin.right + ")")
     .call(zoom);
 
+var container = svg.append("g");
+
+container.append("g")
+    .attr("class", "x axis")
+  .selectAll("line")
+    .data(d3.range(0, width, 10))
+  .enter().append("line")
+    .attr("x1", function(d) { return d; })
+    .attr("y1", 0)
+    .attr("x2", function(d) { return d; })
+    .attr("y2", height);
+
+container.append("g")
+    .attr("class", "y axis")
+  .selectAll("line")
+    .data(d3.range(0, height, 10))
+  .enter().append("line")
+    .attr("x1", 0)
+    .attr("y1", function(d) { return d; })
+    .attr("x2", width)
+    .attr("y2", function(d) { return d; });
+
+
 function zoomed() {
   container.attr("transform", "translate(" + d3.event.translate + ")scale(" + d3.event.scale + ")");
   slider.property("value",  d3.event.scale);
@@ -47,4 +72,7 @@ function dragended(d) {
 function slided(d){
   zoom.scale(d3.select(this).property("value"))
     .event(svg);
+
+  console.log(zoom.scaleExtent()[0])
+
 }
