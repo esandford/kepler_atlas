d669408c6fcc9e6d68e17a7060e54ecfc5d658be
Emily Sandford <esandford@astro.columbia.edu>
2018-05-14 15:54:38 -0400

    Made sliding zoom work!

--- keplerAtlasNB/js/zoom.js
+++ keplerAtlasNB/js/zoom.js
@@ -3,7 +3,7 @@ var margin = {top: -5, right: -5, bottom: -5, left: -5},
     height = 500 - margin.top - margin.bottom;
 
 var zoom = d3.behavior.zoom()
-    .scaleExtent([1, 10])
+    .scaleExtent([0, 10])
     .on("zoom", zoomed);
 
 var drag = d3.behavior.drag()
@@ -52,7 +52,88 @@ container.append("g")
 
 
 function zoomed() {
-  container.attr("transform", "translate(" + d3.event.translate + ")scale(" + d3.event.scale + ")");
+  //we want to use the slider to zoom the viewpoint in and out relative to the current
+  //center of rotation (which is always located at the center of the screen).
+
+  //to do this, we have to:
+  // 1. measure the viewpoint's current distance from the COR;
+  // 2. figure out what "scaling factor" the current distance represents, on our scale from 1 (least zoomed) to 10 (most zoomed);
+  // 3. calculate a new scaling factor based on the slider's position;
+  // 4. divide by the old scaling factor to account for the camera's old position;
+  // 5. multiply by the new scaling factor to move the camera to the new position.
+  
+  //calculate the current distance between the camera and COR
+  var currentPos = viewpoint.attr("position").split(" ");
+  var currentCOR = viewpoint.attr("centerOfRotation").split(" ");
+  console.log(currentPos);
+  console.log(currentCOR);
+  var currentDist_x = currentPos[0] - currentCOR[0];
+  var currentDist_y = currentPos[1] - currentCOR[1];
+  var currentDist_z = currentPos[2] - currentCOR[2];
+  var currentDist = Math.pow((Math.pow(currentDist_x, 2) + Math.pow(currentDist_y, 2) + Math.pow(currentDist_z, 2)), 0.5)
+  
+  //calculate the old scaling factor
+  var galaxyView_pos = [0., 500., 50000.];
+  var earthView_pos = [-117.67830*4.5, -491.90906*4.5, -114.90123*4.5];
+  var maxDist = 0;
+
+  if (view=="galaxy"){
+    for (i=0; i<3; i++){
+      maxDist += Math.pow(galaxyView_pos[i], 2);
+      maxDist = Math.pow(maxDist, 0.5);
+    }
+  }
+  else if (view=="earth"){
+    for (i=0; i<3; i++){
+      maxDist += Math.pow(earthView_pos[i], 2);
+      maxDist = Math.pow(maxDist, 0.5);
+    }
+  }
+
+  if (currentDist > maxDist){
+    currentDist = maxDist;
+  }
+  else if (currentDist < 0){
+    currentDist = 0;
+  }
+
+  console.log("Current dist is: ")
+  console.log(currentDist);
+
+  console.log("Max dist is: ")
+  console.log(maxDist)
+
+  console.log("Dist ratio is: ")
+  console.log(currentDist/maxDist);
+  //calculate the old distance scale
+  var current_scale = (currentDist/maxDist)*10 //number ranging from 0 (least zoomed) to 10 (most zoomed)
+
+  console.log("Current scale is: ")
+  console.log(current_scale);
+
+  //calculate the new scale, based on the slider's current position
+  console.log("Desired scale is: ")
+  var desired_scale = 10.00001 - d3.event.scale; //d3.event.scale is a number ranging from 0.0001 (least zoomed) to 10 (most zoomed);
+                                                //desired_scale is a number ranging from 10 (least zoomed) to 0 (most zoomed)
+  console.log(desired_scale);
+
+  console.log("Scale ratio is: ")
+  console.log(desired_scale/current_scale)
+
+  //calculate the new desired camera position, and move the camera there.
+  var newPos = [];
+  for (i=0; i<3; i++) {
+    console.log(currentPos[i]);
+    console.log(desired_scale/current_scale);
+    console.log(currentPos[i]*(desired_scale/current_scale));
+    newPos.push(currentPos[i]*(desired_scale/current_scale));
+  }
+
+  //move the camera!
+  viewpoint.attr("position", newPos.join(" "));
+
+  console.log(viewpoint.attr("position").split(" "))
+  //give the slider the appropriate value, based on where it was dragged
   slider.property("value",  d3.event.scale);
 }
 
@@ -72,7 +153,4 @@ function dragended(d) {
 function slided(d){
   zoom.scale(d3.select(this).property("value"))
     .event(svg);
-
-  console.log(zoom.scaleExtent()[0])
-
 }
